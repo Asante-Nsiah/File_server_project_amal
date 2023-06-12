152183650bbbcc4adb26d0ea219924e70feb35f7
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2023-06-12 10:16:52 +0000

    Set up database and .env

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
+const port = process.env.PORT || 8000;
+const pool = require('./db');
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! THIS IS MY SECOND PROJECT');
+  });
+  
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ src/db.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'fileServerDB',
+        password: 'postgres',
+        port: 5433,
+      });
+    
+      pool.connect()
+      .then(() => {
+        console.log('Connected to fileServerDB database');
+      })
+      .catch((err) => {
+        console.error('Error connecting to database:', err);
+      });
+
+
+
+
+module.exports = pool;
