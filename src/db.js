e37049bdb6b00e2af7c8cde66157bba38cf4652f
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2023-06-21 10:37:03 +0000

    Connect local psql to render

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-
+const knex = require('knex');
 
     const pool = new Pool({
         user: 'postgres',
@@ -17,7 +17,15 @@ const { Pool } = require('pg');
         console.error('Error connecting to database:', err);
       });
 
-
+      const db = knex({
+        client: 'pg',
+        connection: {
+          connectionString: 'postgres://projectadmin:Y4M0LaKBqXFCLXwmdYvdn3NOI6jJxoEN@dpg-ci661uunqql3q38ihjvg-a/fileserverdb_2s24',
+          ssl: {
+            rejectUnauthorized: false
+          }
+        }
+      });
 
 
 module.exports = pool;
--- src/route/routing.js
+++ src/route/routing.js
@@ -11,13 +11,16 @@ router.post('/signup', auth.signup)
 router.get('/login', auth.login) 
 router.post('/login', auth.loginAccount) 
 router.get('/verify', auth.verify) 
+
 router.get('/user-dashboard', auth.dashboardUser) 
 router.get('/admin-dashboard', auth.dashboardAdmin) 
+
 router.post('/request-resetPd', auth.forgetPassword) 
 router.get('/request-resetPd', auth.requestPd) 
 router.get('/request-resetPd', auth.forgetPassword)
 router.get('/update-password/:token', auth.resetPassword)
 router.post('/update-password', auth.updatePassword)
+
 router.post('/upload', authFile.uploadFile)
 router.get('/upload',  authFile.uploadFile)
 router.get('/download/:filename',  authFile.downloadFile)
